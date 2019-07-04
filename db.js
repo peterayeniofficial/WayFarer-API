4c6841945871ccfe086dbbf5648fb57ca7bcf86f
Peter Ayeni <peterayeni@Peters-MacBook-Pro.local>
2019-07-05 00:05:46 +0100

    feature(api):add created at to user model[start #167097129]

--- db.js
+++ db.js
@@ -12,46 +12,21 @@ pool.on('connect', () => {
 });
 
 /**
- * Create Tables
+ * Create Tables #167097129
  */
 
-const createTables = () => {
+const createUserTable = () => {
     const queryText = `
     CREATE TABLE IF NOT EXISTS
     users(
         id BIGSERIAL PRIMARY KEY,
-        email character varying(150) NOT NULL UNIQUE,
-        first_name character varying(120) NOT NULL,
-        last_name character varying(120) NOT NULL,
-        password character varying(250) NOT NULL,
-        is_admin boolean
-
-        ),
-    buses(
-        id BIGSERIAL PRIMARY KEY,
-        number_plate character varying(12) NOT NULL UNIQUE,
-        manufacturer character varying(100) NOT NULL,
-        model character varying(25) NOT NULL,
-        year date NOT NULL,
-        capacity integer NOT NULL
-    ),
-    trips(
-        id SERIAL PRIMARY KEY,
-        bus_id integer NOT NULL REFERENCES buses(id) ON DELETE CASCADE,
-        origin character varying(100) NOT NULL,
-        destination character varying(100) NOT NULL,
-        trip_date date NOT NULL DEFAULT '2019-07-04'::date,
-        fare numeric NOT NULL,
-        status character varying(10) NOT NULL DEFAULT 'active'::character varying CHECK (status::text = ANY (ARRAY['active'::character varying, 'cancelled'::character varying]::text[]))
-    ),
-    bookings(
-        id BIGSERIAL PRIMARY KEY,
-        email character varying(150) NOT NULL UNIQUE,
-        first_name character varying(120) NOT NULL,
-        last_name character varying(120) NOT NULL,
-        password character varying(250) NOT NULL,
-        is_admin boolean
-    )`;
+        first_name VARCHAR(120) NOT NULL,
+        last_name VARCHAR(120) NOT NULL,
+        email VARCHAR(150) NOT NULL UNIQUE,
+        password VARCHAR(250) NOT NULL,
+        is_admin boolean,
+        created_on timestamp
+        )`;
 
     pool.query(queryText)
         .then((res) => {
@@ -64,8 +39,85 @@ const createTables = () => {
         });
 };
 
-const dropTables = () => {
-    const queryText = 'DROP TABLE IF EXISTS users, busses, trips, bookings';
+const createBusTable = () => {
+    const queryText = `
+    CREATE TABLE IF NOT EXISTS
+    buses(
+        id BIGSERIAL PRIMARY KEY,
+        number_plate VARCHAR(12) NOT NULL UNIQUE,
+        manufacturer VARCHAR(100) NOT NULL,
+        model VARCHAR(25) NOT NULL,
+        year date NOT NULL,
+        capacity integer NOT NULL
+    )`;
+    pool.query(queryText)
+        .then((res) => {
+            console.log(res);
+            pool.end();
+        })
+        .catch((err) => {
+            console.log(err);
+            pool.end();
+        });
+};
+
+const createTripTable = () => {
+    const queryText = `
+    CREATE TABLE IF NOT EXISTS
+    trips(
+        id BIGSERIAL PRIMARY KEY,
+        bus_id integer NOT NULL REFERENCES buses(id) ON DELETE CASCADE,
+        origin VARCHAR(100) NOT NULL,
+        destination VARCHAR(100) NOT NULL,
+        trip_date timestamp,
+        fare numeric NOT NULL,
+        status VARCHAR(10) NOT NULL DEFAULT 'active'::VARCHAR CHECK (status::text = ANY (ARRAY['active'::character varying, 'cancelled'::character varying]::text[]))
+    )`;
+    pool.query(queryText)
+        .then((res) => {
+            console.log(res);
+            pool.end();
+        })
+        .catch((err) => {
+            console.log(err);
+            pool.end();
+        });
+};
+
+const createBookingTable = () => {
+    const queryText = `
+    CREATE TABLE IF NOT EXISTS
+    bookings(
+        id BIGSERIAL PRIMARY KEY,
+        trip_id integer NOT NULL REFERENCES trips(id) ON DELETE CASCADE,
+        user_id integer NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+        created_on timestamp
+    )`;
+    pool.query(queryText)
+        .then((res) => {
+            console.log(res);
+            pool.end();
+        })
+        .catch((err) => {
+            console.log(err);
+            pool.end();
+        });
+};
+
+const createAllTables = () => {
+    createUserTable();
+    createBusTable();
+    createTripTable();
+    createBookingTable();
+};
+
+
+/**
+ * Drop Tables #167097129
+ */
+
+const dropUserTable = () => {
+    const queryText = 'DROP TABLE IF EXISTS users';
     pool.query(queryText)
         .then((res) => {
             console.log(res);
@@ -82,9 +134,71 @@ pool.on('remove', () => {
     process.exit(0);
 });
 
+const dropBusTable = () => {
+    const queryText = 'DROP TABLE IF EXISTS buses';
+    pool.query(queryText)
+        .then((res) => {
+            console.log(res);
+            pool.end();
+        })
+        .catch((err) => {
+            console.log(err);
+            pool.end();
+        });
+};
+
+pool.on('remove', () => {
+    console.log('client removed');
+    process.exit(0);
+});
+
+const dropTripTable = () => {
+    const queryText = 'DROP TABLE IF EXISTS trips';
+    pool.query(queryText)
+        .then((res) => {
+            console.log(res);
+            pool.end();
+        })
+        .catch((err) => {
+            console.log(err);
+            pool.end();
+        });
+};
+
+pool.on('remove', () => {
+    console.log('client removed');
+    process.exit(0);
+});
+
+const dropBookingTable = () => {
+    const queryText = 'DROP TABLE IF EXISTS bookings';
+    pool.query(queryText)
+        .then((res) => {
+            console.log(res);
+            pool.end();
+        })
+        .catch((err) => {
+            console.log(err);
+            pool.end();
+        });
+};
+
+pool.on('remove', () => {
+    console.log('client removed');
+    process.exit(0);
+});
+
+const dropAllTables = () => {
+    dropUserTable();
+    dropBusTable();
+    dropTripTable();
+    dropBookingTable();
+};
+
 module.exports = {
-    createTables,
-    dropTables,
+    createAllTables,
+    dropAllTables,
+    createBookingTable,
 };
 
 require('make-runnable');
