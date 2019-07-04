aa2772aa11d3fab7b3942b0f5c3b51183e870907
Peter Ayeni <peterayeni@Peters-MacBook-Pro.local>
2019-07-04 15:03:13 +0100

    feature(database):create users table[start #167089900]

--- db.js
+++ db.js
@@ -10,3 +10,56 @@ const pool = new Pool({
 pool.on('connect', () => {
     console.log('connected to the db successfully');
 });
+
+/**
+ * Create Tables
+ */
+
+const createTables = () => {
+    const queryText = `
+    CREATE TABLE IF NOT EXISTS
+    users(
+        id BIGSERIAL NOT NULL PRIMARY KEY,
+        email VARCHAR(150) NOT NULL UNIQUE,
+        first_name VARCHAR(120) NOT NULL,
+        last_name VARCHAR(120) NOT NULL,
+        password VARCHAR(250) NOT NULL,
+        is_admin BOOLEAN
+
+        )`;
+
+    pool.query(queryText)
+        .then((res) => {
+            console.log(res);
+            pool.end();
+        })
+        .catch((err) => {
+            console.log(err);
+            pool.end();
+        });
+};
+
+const dropTables = () => {
+    const queryText = 'DROP TABLE IF EXISTS users';
+    pool.query(queryText)
+        .then((res) => {
+            console.log(res);
+            pool.end();
+        })
+        .catch((err) => {
+            console.log(err);
+            pool.end();
+        });
+};
+
+pool.on('remove', () => {
+    console.log('client removed');
+    process.exit(0);
+});
+
+module.exports = {
+    createTables,
+    dropTables,
+};
+
+require('make-runnable');
